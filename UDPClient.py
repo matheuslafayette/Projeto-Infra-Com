e4e3eb0bcc06615d896dad3f4d0ce1e72744a7be
matheuslafayette <matheuslafayett@gmail.com>
2022-08-28 21:18:54 -0300

    server receiving

--- UDPClient.py
+++ UDPClient.py
@@ -1,26 +1,21 @@
 from socket import *
-import os
 
 serverName = '10.0.0.112'
 serverPort = 12000
 
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 
-filename = "TestePDF.pdf"
+filename = "TesteTXT.txt"
 pathfile = "./arquivos-teste/" + filename
-
-file = open(pathfile)
 
 clientSocket.sendto(f"{filename}".encode(), (serverName, serverPort))
 
 with(open(pathfile, "rb")) as f:
     while True:
         bytes_read = f.read(2048)
-        if bytes_read:
-            clientSocket.sendto(bytes_read, (serverName, serverPort))
-        else:
+        clientSocket.sendto(bytes_read, (serverName, serverPort))
+        if not bytes_read:
             f.close()
             break
-        
 
 clientSocket.close()
--- UDPServer.py
+++ UDPServer.py
@@ -5,20 +5,17 @@ serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(("", serverPort))
 print("The server is ready to receive")
 
-received = serverSocket.recv(2048)
-filename = received.decode()
-filename = "./arquivos-teste/2-" + filename
-
-with open(filename, "wb") as newFile:
-    while True:
-        bytes_read = serverSocket.recv(2048)
-        if bytes_read:
-            newFile.write(bytes_read)
-        else:
-            newFile.close()
-            break
+while True:
     
-    # filesize = int(filesize)
-    # modifiedFile = file.decode().upper()
-    # serverSocket.sendto(modifiedFile.encode(), 
-    #                     clientAddress)
+    received = serverSocket.recv(2048)
+    filename = received.decode()
+    filename = "2-" + filename
+    pathfile = "./arquivos-teste/" + filename
+
+    with open(pathfile, "wb") as newFile:
+        while True:
+            bytes_read = serverSocket.recv(2048)
+            if not bytes_read:
+                newFile.close()
+                break
+            newFile.write(bytes_read)
