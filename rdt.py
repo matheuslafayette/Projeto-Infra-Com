9c135c01f378abe354c14d1cb1ded3619da982c5
matheuslafayette <matheuslafayett@gmail.com>
2022-09-29 00:30:37 -0300

    added timeout

--- rdt.py
+++ rdt.py
@@ -16,10 +16,14 @@ class Rdt:
     def __del__(self):
         self.socket.close()
     
+    def reset_num_seq(self):
+        self.num_seq_c = 0
+        self.num_seq_s = 0
+    
     def close(self):
         self.__del__()
     
-    def send(self, data):
+    def udt_send(self, data):
         
         if self.type == 'client':
             addr = (self.serverName, self.serverPort)
@@ -29,15 +33,15 @@ class Rdt:
         if not isinstance(data, bytes):
             data = data.encode()
         
-        print(data)
+        #print(data)
         
         self.socket.sendto(data, addr)
     
-    def receive(self):
+    def udt_rcv(self):
         if(self.type == 'client'):
-            bytes_read = self.socket.recv(2048)
+            bytes_read = self.socket.recv(4096)
         else:
-            bytes_read, self.serverName = self.socket.recvfrom(2048)
+            bytes_read, self.serverName = self.socket.recvfrom(4096)
         return bytes_read
     
     def rdt_send(self, data):
@@ -46,37 +50,38 @@ class Rdt:
             data = data.encode()
         
         sum = checksum(data)
-            
         sndpkt = make_pkt(data, sum, self.num_seq_c)
         sndpkt.encode()
-        
-        self.send(sndpkt)
-        rcvpkt = self.rdt_rcv('wait_ack')
-        
+
+        rcvpkt = None
         while(not rcvpkt or corrupt(rcvpkt) or rcvpkt['num_seq'] != self.num_seq_c or not rcvpkt['is_ack']):
-            self.send(sndpkt)
-            rcvpkt = self.rdt_rcv('wait_ack')
+            self.socket.settimeout(1)
+            self.udt_send(sndpkt)
+            try:
+                rcvpkt = self.rdt_rcv('wait_ack')
+            except socket.timeout:
+                continue
+            else:
+                self.socket.settimeout(None)
+                break
         
         self.num_seq_c = 1 - self.num_seq_c
     
     def rdt_rcv(self, state : str = 'null'):
         
         if(state == 'wait_ack'):
-            rcvpkt = eval(self.receive().decode())
-            
+            rcvpkt = None
             while(not rcvpkt or corrupt(rcvpkt) or rcvpkt['num_seq'] != self.num_seq_c):
-                rcvpkt = eval(self.receive().decode())
-            
+                rcvpkt = eval(self.udt_rcv().decode())
             
         else:
-            rcvpkt = eval(self.receive().decode())
-            
+            rcvpkt = None
             while(not rcvpkt or corrupt(rcvpkt) or rcvpkt['num_seq'] != self.num_seq_s):
-                rcvpkt = eval(self.receive().decode())
+                rcvpkt = eval(self.udt_rcv().decode())
                 
             sndack = make_ack(self.num_seq_s)
-            self.send(sndack)
-            print('mandei ack')
+            self.udt_send(sndack)
+            #print('send ack')
             self.num_seq_s = 1 - self.num_seq_s
         
         return rcvpkt
