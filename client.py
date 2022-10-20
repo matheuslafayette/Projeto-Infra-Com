487315da7105a1ae25db2013b8c2758828a4a3d6
matheuslafayette <matheuslafayett@gmail.com>
2022-10-20 16:48:44 -0300

    new client

--- client.py
+++ client.py
@@ -1,36 +1,27 @@
 from rdt import *
+import time
 
 def main():
     
     client = Rdt('client')
+    username = 'user'
+    state = False
     
-    filename = input("nome do arquivo: ")
-    pathfile = "./arquivos-teste/" + filename
-    
-    client.rdt_send(filename)
-    
-    with(open(pathfile, "rb")) as file:
-        while True:
-            bytes_read = file.read(1024)
-            client.rdt_send(bytes_read)
-            if not bytes_read:
-                file.close()
+    while True:
+                
+        if not state:
+            msg = input('mensagem: ')
+            #client.rdt_send(msgsend)
+            
+            if msg == "bye":
+                client.close()
                 break
-    
-    print("arquivo enviado para o server")
-    
-    filename = "client-" + filename
-    pathfile = "./client/" + filename
-    
-    with open(pathfile, "wb") as newFile:
-        while True:
-            bytes_read = client.rdt_rcv()['data']
-            if not bytes_read:
-                newFile.close()
-                break   
-            newFile.write(bytes_read)
-    
-    print("arquivo recebido do server")
+            
+            state = ~state
+        
+        else:
+            #receive from server
+            True
 
 if __name__ == "__main__":
     main()
